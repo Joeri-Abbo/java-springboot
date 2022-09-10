7756e792e1a6818be182f5115c04b742f3c469be
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-11 01:00:50 +0200

    Icttrainingen : Caching using @Cacheable.

--- src/main/java/com/skillsoft/springboot/service/ProductService.java
+++ src/main/java/com/skillsoft/springboot/service/ProductService.java
@@ -3,6 +3,7 @@ package com.skillsoft.springboot.service;
 import com.skillsoft.springboot.model.Product;
 import com.skillsoft.springboot.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,29 +16,55 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Cacheable("products")
     public List<Product> getAllProducts() {
         List<Product> products = new ArrayList<>();
-
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         productRepository.findAll().forEach(products::add);
 
         return products;
     }
 
+    @Cacheable(value = "product", key = "#p0")
     public Optional<Product> getProduct(Long id) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return productRepository.findById(id);
     }
 
     public void addProduct(Product product) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         productRepository.save(product);
     }
 
     public void updateProduct(Long id, Product product) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         if (productRepository.findById(id).get() != null) {
             productRepository.save(product);
         }
     }
 
     public void deleteProduct(Long id) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         productRepository.deleteById(id);
     }
 }
