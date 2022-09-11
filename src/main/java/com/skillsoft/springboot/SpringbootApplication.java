5db14677fec95ff7627355077f63dd7f3c5db3bd
Joeri Abbo <joeriabbo@hotmail.com>
2022-09-11 14:49:43 +0200

    ICTtrainingen : Setting up Zuul Edge Service.

--- src/main/java/com/skillsoft/springboot/SpringbootApplication.java
+++ src/main/java/com/skillsoft/springboot/SpringbootApplication.java
@@ -2,13 +2,23 @@ package com.skillsoft.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableCaching
+@RestController
 public class SpringbootApplication {
+    @RequestMapping(value = "/search")
+    public String search() {
+        return "<h3>Searching for a product</h3>";
+    }
+
+    @RequestMapping(value = "/order")
+    public String order() {
+        return "<h3>Placing a product order</h3>";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
-
 }
